daab56499193674a2e26a68c3b45d9b6938ab21c
Iphigenie Gonnet <43339184+iphigenieG@users.noreply.github.com>
2023-01-15 17:57:31 +0100

    Update immuneSIMusage.R

--- src/immuneSIMusage.R
+++ src/immuneSIMusage.R
@@ -1,19 +1,21 @@
 library(immuneSIM)
+v_usage_fpath <- "projet_long/donnees/v_usage.txt"
+j_usage_fpath <- "projet_long/donnees/j_usage.txt"
 # basic usage of immuneSIM for TCR
 # sim_tcr = immuneSIM(number_of_seqs = 200, species = "hs", receptor = "tr", chain= "b")
-# plot_report_repertoire(sim_tcr,output_dir = "~/Documents/projet_long")
+# plot_report_repertoire(sim_tcr,output_dir = "projet_long/res")
 
 # how to change the frequencies of genes to match that of our data
 
 # list_germline_genes_allele_01_Teff_mus <- list_germline_genes_allele_01
 # 
-# v_usage = read.csv("~/Documents/projet_long/donnees/v_usage.txt",sep=" ",header = TRUE)
+# v_usage = read.csv(v_usage_fpath,sep=" ",header = TRUE)
 # list_germline_genes_allele_01_Teff_mus$mm$tr$b$V <- subset(
 #   list_germline_genes_allele_01_Teff_mus$mm$tr$b$V,
 #   list_germline_genes_allele_01_Teff_mus$mm$tr$b$V$gene %in% row.names(v_usage))
 # list_germline_genes_allele_01_Teff_mus$mm$tr$b$V$frequency <- v_usage$Teff
 # 
-# j_usage = read.csv("~/Documents/projet_long/donnees/j_usage.txt",sep=" ",header = TRUE)
+# j_usage = read.csv(j_usage_fpath,sep=" ",header = TRUE)
 # list_germline_genes_allele_01_Teff_mus$mm$tr$b$J <- subset(
 #   list_germline_genes_allele_01_Teff_mus$mm$tr$b$J,
 #   list_germline_genes_allele_01_Teff_mus$mm$tr$b$J$gene %in% row.names(j_usage))
@@ -23,13 +25,13 @@ library(immuneSIM)
 
 list_germline_genes_allele_01_Treg_mus <- list_germline_genes_allele_01
 
-v_usage = read.csv("~/Documents/projet_long/donnees/v_usage.txt",sep=" ",header = TRUE)
+v_usage = read.csv(v_usage_fpath,sep=" ",header = TRUE)
 list_germline_genes_allele_01_Treg_mus$mm$tr$b$V <- subset(
   list_germline_genes_allele_01_Treg_mus$mm$tr$b$V,
   list_germline_genes_allele_01_Treg_mus$mm$tr$b$V$gene %in% row.names(v_usage))
 list_germline_genes_allele_01_Treg_mus$mm$tr$b$V$frequency <- v_usage$amTreg
 
-j_usage = read.csv("~/Documents/projet_long/donnees/j_usage.txt",sep=" ",header = TRUE)
+j_usage = read.csv(j_usage_fpath,sep=" ",header = TRUE)
 list_germline_genes_allele_01_Treg_mus$mm$tr$b$J <- subset(
   list_germline_genes_allele_01_Treg_mus$mm$tr$b$J,
   list_germline_genes_allele_01_Treg_mus$mm$tr$b$J$gene %in% row.names(j_usage))
